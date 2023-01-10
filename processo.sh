22c6a6e0c8034a94d8c8dc06a71793aa7bf129c3
Leonardo Ferreira <73661429+Leonardo-novais@users.noreply.github.com>
2023-01-10 17:11:14 -0300

    Update processo.sh

--- processo.sh
+++ processo.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
+ps aux | sort -nk 4 -r | head -n 1
 
-$ ps axo rss,comm,pid \
-| awk '{ proc_list[$2]++; proc_list[$2 "," 1] += $1; } \
-END { for (proc in proc_list) { printf("%d\t%s\n", \
-proc_list[proc "," 1],proc); }}' \
-| sort -n \
-| tail -n 10 \
-| sort -rn \
-| awk '{$1/=1024;printf "%.0fMB\t",$1}{print $2}'
+Para obter com cabeçalho:
+ps aux |head -1 ; ps aux | sort -nk 4 -r | head -n 1
